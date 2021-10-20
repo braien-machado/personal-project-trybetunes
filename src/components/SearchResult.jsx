7b55dd92c047b6a5c8d4efae89c4dac786695bb5
Braien <braienmp@outlook.com>
2021-10-20 15:21:56 -0300

    renderiza albuns com link para /album/:id

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class SearchResult extends Component {
   render() {
@@ -8,6 +9,23 @@ export default class SearchResult extends Component {
       return (
         <div>
           <h3>{ `Resultado de álbuns de: ${name}`}</h3>
+          <div>
+            {result.map((album) => {
+              const { artistName, collectionId, collectionName, artworkUrl100 } = album;
+              return (
+                <div key={ collectionId } className="album-container">
+                  <img src={ artworkUrl100 } alt={ `${collectionName} cover` } />
+                  <Link
+                    to={ `/album/${collectionId}` }
+                    data-testid={ `link-to-album-${collectionId}` }
+                  >
+                    <h3>{ collectionName }</h3>
+                  </Link>
+                  <h4>{ artistName }</h4>
+                </div>
+              );
+            })}
+          </div>
         </div>
       );
     }
